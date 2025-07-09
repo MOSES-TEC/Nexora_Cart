a458b9bbf48b1f737951d78b643697f24dca0608
MOSES Y <ymoses546@gmail.com>
2025-07-09 21:01:42 +0530

    impl of auth

--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -10,13 +10,13 @@ const authUser = async (req, res, next) => {
     try {
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
         if(tokenDecode.id) {
-            // req.body.userId = tokenDecode.id;
+            req.body.userId = tokenDecode.id;
             req.user = { id: tokenDecode.id }; 
             next();
         } else {
             return res.json({ success: false, message: 'Not Authorized'});
         }
-        // next();
+        next();
 
     } catch (error) {
         res.json({ success: false, message: error.message });   
