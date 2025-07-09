0ed604a5f25735ebc6e770fb7abf65e5a113a40f
MOSES Y <ymoses546@gmail.com>
2025-07-09 21:05:30 +0530

    impl of user

--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -12,7 +12,7 @@ const authUser = async (req, res, next) => {
         if(tokenDecode.id) {
             req.body.userId = tokenDecode.id;
             req.user = { id: tokenDecode.id }; 
-            next();
+            // next();
         } else {
             return res.json({ success: false, message: 'Not Authorized'});
         }
