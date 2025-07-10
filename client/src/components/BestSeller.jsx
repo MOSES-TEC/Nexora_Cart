20be78c95364cfdef1342c769500a3a75a4b3ed6
MOSES Y <ymoses546@gmail.com>
2025-07-11 00:02:33 +0530

    impl of best products

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -8,8 +8,8 @@ const BestSeller = () => {
 
   return (
     <div className="mt-16">
-        <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
-        <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 sm:gap-4 md:gap-6">
+        <p className="text-2xl md:text-3xl font-medium pb-4">Best Sellers</p>
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 sm:gap-4 md:gap-6 px-2 sm:px-0">
             {products.filter((product) => product.inStock).slice(0, 5).map((product, index) => (
                 <ProductCard key={index} product={product} />
             ))}
